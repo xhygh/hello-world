ea24c10de4b1addb48753be1031c004622b90c2c
xhygh <xuhaiyue_bj@163.com>
2016-09-01 23:09:10 +0800

    code

---
+++ 326. Power of Three.py
@@ -0,0 +1,16 @@
+"""
+Given an integer, write a function to determine if it is a power of three.
+
+Follow up:
+Could you do it without using any loop / recursion?
+"""
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if not n or n<0: return False
+        #tmp = math.log(n,3)可能有n=243时，tmp=4.999999999,如此，int(tmp)=4
+        tmp = math.log10(n) / math.log10(3)
+        return True if tmp - int(tmp)==0 else False
