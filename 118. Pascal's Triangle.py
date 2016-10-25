4f7971b5820264f6b46eab6f4e4d2d7d08092586
xhygh <xuhaiyue_bj@163.com>
2016-10-25 23:21:30 +0800

    Update 118. Pascal's Triangle.py
    map()
    排列特性

--- 118. Pascal's Triangle.py
+++ 118. Pascal's Triangle.py
@@ -36,6 +36,21 @@ class Solution(object):
         return results
         
         # approach2
-        
-        
+        # 可以先生成每行形式，在替换中间的元素
+        res = []
+        for i in range(0,numRows):
+            res.append([1]*(i+1))
+            for j in range(1,i):
+                res[i][j] = res[i-1][j-1] + res[i-1][j]
+        return res
         # approach3
+        # 每行两边是1，res[i][j]=res[i-1][j-1]+res[i-1][j],也可以理解为i-1行与其副本相错一个位置相加的结果。
+        # 1 3 3 1 0 +
+        # 0 1 3 3 1
+        #=1 4 6 4 1
+        # 因为是对称的，所以正反相加就可以
+        if numRows == 0: return[]
+        res=[[1]]
+        for i in range(1,numRows):
+            res += [map(lambda x,y:x+y,res[-1]+[0],[0]+res[-1])]
+        return res
