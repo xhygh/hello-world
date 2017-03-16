7febd4872a1093477557f236aa386e9e9a25ca7b
xhygh <xuhaiyue_bj@163.com>
2017-03-16 12:57:23 +0800

    【tags】Linked List 归并【similar problems】23h,88e,148m
    【tags】Linked List 归并【similar problems】23h,88e,148m

--- leetcode-cpp/21. Merge Two Sorted Lists.cpp
+++ leetcode-cpp/21. Merge Two Sorted Lists.cpp
@@ -133,3 +133,45 @@ public:
         return res.next;
     }
 };
+
+
+/******************
+方法3,：
+很清晰的表达了归并算法的原理
+208 / 208 test cases passed.
+Status: Accepted
+Runtime: 12 ms
+时间复杂度O(min(m,n)),空间复杂度O(1)
+*******************/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1==nullptr) return l2;
+        if(l2==nullptr) return l1;
+        ListNode res(-1);
+        ListNode* rear = &res; 
+        
+        for(;l1!=nullptr && l2!=nullptr;rear=rear->next){
+            if(l1->val<=l2->val){
+                rear->next = l1;
+                l1 = l1->next;//如果l1->next==nullptr也可以赋值成功，赋值nullptr
+            }
+            else{
+                rear->next = l2;
+                l2 = l2->next;
+            }
+        }
+        rear->next = l1!=nullptr?l1:l2;
+        return res.next;
+    }
+};
