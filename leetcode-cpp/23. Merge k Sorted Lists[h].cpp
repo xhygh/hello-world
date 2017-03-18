acf9a2a07209f751eeda520b29d3c88f4a633871
xhygh <xuhaiyue_bj@163.com>
2017-03-18 21:45:40 +0800

    divide and conquer,linked list, heap;21e,263m
    tags: divide and conquer,linked list, heap;21e,263m

---
+++ leetcode-cpp/23. Merge k Sorted Lists[h].cpp
@@ -0,0 +1,14 @@
+/*
+
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+
+tags: divide and conquer,linked list, heap;21e,263m
+
+*/
+
+
+/********************
+解题思路：
+开始当然想复用21e的解法，将其他list都并入到第一个中，但是题解说超时。
+所以想到只能k个一起来了，超时的话再说的。
+**********************/
