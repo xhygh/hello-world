59500cb190eb822be56bc8a947e34a38b8bfa36d
xhygh <xuhaiyue_bj@163.com>
2017-04-13 23:57:18 +0800

    tags:tree, depth-first search;109m
    tags:tree, depth-first search;109m

---
+++ leetcode-cpp/108. Convert Sorted Array to Binary Search Tree[e].cpp
@@ -0,0 +1,42 @@
+/*
+Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+
+tags:tree, depth-first search;109m
+*/
+
+/***************
+solution:
+二分法，因为BST的定义是左边<root<右边。
+
+32 / 32 test cases passed.
+Status: Accepted
+Runtime: 13 ms
+78.86%
+o(n),O(1)
+***************/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return BST(nums,0,nums.size()); //[start,end)范围
+    }
+    
+    TreeNode* BST(vector<int>& nums,int start,int end){
+        if(end-start<=0) return NULL;
+        int mid = (end+start)/2;
+        TreeNode* root = new TreeNode(nums[mid]);
+        root->left = BST(nums,start,mid);
+        root->right = BST(nums,mid+1,end);
+        
+        return root;
+    }
+};
