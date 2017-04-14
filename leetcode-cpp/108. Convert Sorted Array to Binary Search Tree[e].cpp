090500623d46a05bf967136c241f80c02655dc37
xhygh <xuhaiyue_bj@163.com>
2017-04-14 10:22:24 +0800

    tags:tree, depth-first search,[binary search];109m
    tags:tree, depth-first search,[binary search];109m

--- leetcode-cpp/108. Convert Sorted Array to Binary Search Tree[e].cpp
+++ leetcode-cpp/108. Convert Sorted Array to Binary Search Tree[e].cpp
@@ -40,3 +40,34 @@ public:
         return root;
     }
 };
+ 
+
+/*****************
+solution:
+[start,end]范围的二分法
+*********************/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return BST(nums,0,nums.size()-1); // 范围[start,end]
+    }
+    
+    TreeNode* BST(vector<int>& nums,int start,int end){
+        if(end-start<0) return NULL;
+        int mid = (end+start)/2;
+        TreeNode* root = new TreeNode(nums[mid]);
+        root->left = BST(nums,start,mid-1);// 范围[start,mid-1]
+        root->right = BST(nums,mid+1,end);
+        
+        return root;
+    }
+};
