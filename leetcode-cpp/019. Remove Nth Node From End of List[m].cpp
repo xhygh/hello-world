1a08f00b955c00097daa5ce8a0eb2dc37fde2c04
xhygh <xuhaiyue_bj@163.com>
2017-05-26 22:21:46 +0800

    tags:linked list;two pointers
    tags:linked list;two pointers

---
+++ leetcode-cpp/019. Remove Nth Node From End of List[m].cpp
@@ -0,0 +1,49 @@
+/*
+Given a linked list, remove the nth node from the end of list and return its head.
+
+For example,
+
+   Given linked list: 1->2->3->4->5, and n = 2.
+
+   After removing the second node from the end, the linked list becomes 1->2->3->5.
+
+Note:
+Given n will always be valid.
+Try to do this in one pass. 
+
+
+tags:linked list;two pointers
+*/
+/*
+将链表的右边数第n个节点移除
+solution：
+链表删除，快慢指针
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if (n<=0) return head;
+        ListNode *faster=head,*smaller=head;
+        for(int i=0;i<n;i++){
+            if (faster!=nullptr)
+                faster = faster->next;
+            else
+                return head;
+        }
+        while(faster){
+            smaller=smaller->next;
+            faster=faster->next;
+        }
+        smaller->next = n==1?nullptr:smaller->next->next;
+        return head;
+        
+    }
+};
